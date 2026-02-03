0c68b5b2e94f56b12673ec1b1bc0ba06ead2eacb
Andrian <andrian.svezhenovnikolaev@hashicorp.com>
2026-02-03 14:56:08 +0200

    dsa

---
+++ variables.tf
@@ -0,0 +1,69 @@
+variable "vpc_name" {
+  type    = string
+  default = null
+}
+
+variable "environment_name" {
+  type    = string
+  default = null
+}
+variable "vpc_cidr_block" {
+  type    = string
+  default = null
+}
+variable "subnet_option" {
+  type    = string
+  default = null
+}
+variable "region" {
+  type    = string
+  default = null
+}
+
+variable "create_tgw_attachment" {
+  type    = bool
+  default = null
+}
+variable "auto_accept_tgw_attachment" {
+  type    = bool
+  default = null
+}
+variable "tgw_id" {
+  type    = string
+  default = null
+}
+variable "tgw_route_table_id" {
+  type    = string
+  default = null
+}
+variable "tgw_vpn_propagation_route_table_ids" {
+  type    = list(string)
+  default = []
+}
+
+variable "create_egress_route" {
+  type    = bool
+  default = null
+}
+variable "create_peer_connection" {
+  type    = bool
+  default = null
+}
+
+variable "create_r53_zone_association" {
+  type    = bool
+  default = null
+}
+variable "r53_hosted_zone_ids" {
+  type    = list(string)
+  default = []
+}
+variable "create_r53_rule_association" {
+  type    = bool
+  default = null
+}
+
+variable "create_vpc_endpoints" {
+  type    = bool
+  default = null
+}
